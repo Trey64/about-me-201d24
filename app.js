f2ee2f1aa582c3931640dc2a1f745358e31006fc
Joseph Kim <joseph.kim.kr@gmail.com>
2017-07-13 17:01:22 -0700

    Added functions for structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict'
 
 var username = prompt('What is your name?');
 alert('Hey there, ' + username + '. Enjoy this game!');
@@ -7,141 +7,157 @@ console.log('The user said their name is ' + username);
 // Blank array. New items will be pushed with each correct answer
 var questions = [];
 
+// Functions
 
-var game = prompt('My current favorite game is Overwatch. Yes or No?').toLowerCase();
-if (game === 'yes' || game === 'y') {
-  alert('Yes it is. D.Va is the best!');
-  console.log('The user guessed ' + game + ' correctly');
-  questions.push(1);
-} else if (game === 'no' || game === 'n') {
-    alert('Wrong, it is!');
-    console.log('The user guessed ' + game + ' incorrectly');
-} else {
-    alert('Please answer Yes or No');
-    console.log('The user entered ' + game + ', an invalid string');
-}
-
-
-var sister = prompt('I have a sister. Yes or No?').toLowerCase();
-if (sister === 'yes' || sister === 'y') {
-  alert('Yep, I have a younger sister.');
-  questions.push(2);
-  console.log('The user guessed ' + sister + ' correctly');
-
-} else if (sister === 'no' || sister === 'n') {
-    alert('Yes, I do! I have a younger sister.');
-    console.log('The user guessed ' + sister + ' incorrectly');
-} else {
-    alert('Please answer Yes or No');
-    console.log('The user entered ' + sister + ',  an invalid string');
-}
-
-
-var car = prompt('I drive a Jeep. Yes or No?').toLowerCase();
-if (car === 'no' || car === 'n') {
-  alert('You\'re right, I actually drive a Volkswagen.');
-  questions.push(3);
-  console.log('The user guessed ' + car + ' correctly');
-} else if (car === 'yes' || car === 'y') {
-    alert('I don\'t, but Jeeps are still cool.');
-    console.log('The user guessed ' + car + ' incorrectly');
-} else {
-    alert('Please answer Yes or No');
-    console.log('The user entered ' + car + ',  an invalid string');
-}
-
-
-var country = prompt('I have been to South America. Yes or No?').toLowerCase();
-if (country === 'yes' || country === 'y') {
-  alert('Correct! I\'ve been to Panama, Peru, and Mexico.');
-  console.log('The user guessed ' + country + ' correctly');
-  questions.push(4);
-} else if (country === 'no' || country === 'n') {
-    alert('Wrong! I\'ve been to Panama, Peru, and Mexico.');
-    console.log('The user guessed ' + country + ' incorrectly');
-} else {
-    alert('Please answer Yes or No');
-    console.log('The user entered ' + country + ',  an invalid string');
-}
-
-
-var celebrity = prompt('I once went on a date with Gal Gadot. Yes or No?').toLowerCase();
-if (celebrity === 'no' || celebrity === 'n') {
-  alert('Ok ' + username + ', you got me. But a guy can dream, right??');
-  questions.push(5);
-  console.log('The user guessed ' + celebrity + ' correctly');
-} else if (celebrity === 'yes' || celebrity === 'y') {
-    alert('Nope, but thanks for thinking I have a chance, ' + username + '!');
-    console.log('The user guessed ' + celebrity + ' incorrectly');
-} else {
-    alert('Please answer Yes or No');
-    console.log('The user entered ' + celebrity + ',  an invalid string');
-}
-
-//Question 6
-var attempts = 4;
-while (attempts > 0)
-  {
-    var number = parseInt(prompt('I\'m thinking of a number between 1 and 10, what is it?'));
-    if (number === 8) {
-      alert('That\'s right!');
-      questions.push(6);
-      console.log('The user guessed correctly');
-      break;
-    } else if (number > 8) {
-      alert('Too high!');
-      attempts--;
-      console.log('The user guessed ' + number + ', which is too high');
-    } else if (number < 8) {
-      alert('Too low!');
-      attempts--;
-      console.log('The user guessed ' +number + ', which is too low');
+function checkAnswer(response, answer, rightMsg, wrongMsg) {
+  if (answer === 'yes') {
+    if (response === 'yes' || response === 'y') {
+      alert(rightMsg);
+      questions.push('marker');
+      console.log('The user guessed ' + response + ' correctly');
+    } else if (response === 'no' || response === 'n') {
+      alert(wrongMsg);
+      console.log('The user guessed ' + response + ' incorrectly');
     } else {
-      alert('Please enter a number');
-      attempts--;
-      console.log('The user entered an invalid string');
+      alert('Please answer Yes or No');
+      console.log('The user entered ' + response + ', an invalid string');
     }
-    if (attempts === 0) {
-      alert('Oh no, you ran out of guesses!');
-      console.log('The user used up all their attempts');
+  } else if (answer === 'no') {
+    if (response === 'no' || response === 'n') {
+      alert(rightMsg);
+      questions.push('marker');
+      console.log('The user guessed ' + response + ' correctly');
+    } else if (response === 'yes' || response === 'y') {
+      alert(wrongMsg);
+      console.log('The user guessed ' + response + ' incorrectly');
+    } else {
+      alert('Please answer Yes or No');
+      console.log('The user entered ' + response + ', an invalid string');
     }
   }
+}
 
-//Question 7
-var heroes = ['batman', 'superman', 'wonder woman', 'the flash'];
-var guesses = 6;
+// Question 1
+function question1() {
+  var game = prompt('My current favorite game is Overwatch. Yes or No?').toLowerCase();
+  var q1Right = 'Yes it is. D.Va is the best!';
+  var q1Wrong = 'Wrong, it is!';
+  checkAnswer(game, 'yes', q1Right, q1Wrong);
+}
+question1();
 
-while (guesses > 0) {
-    var justice = prompt('Can you guess one of my favorite members of the Justice League?').toLowerCase();
+ // Question 2
+function question2() {
+  var sister = prompt('I have a sister. Yes or No?').toLowerCase();
+  var q2Right = 'Yep, I have a younger sister.';
+  var q2Wrong = 'Yes, I do! I have a younger sister.';
+  checkAnswer(sister, 'yes', q2Right, q2Wrong);
+}
+question2();
 
-    for (var i = 0; i < heroes.length; i++) {
-      if (justice === heroes[i] || justice === heroes[1] || justice === heroes[2] || justice === heroes[3]) {
-        alert('Correct! My favorites are Batman, Superman, Wonder Woman, and The Flash.');
-        questions.push(7);
-        console.log('User guessed ' + justice + ', which is part of ' + heroes + i);
-        guesses = 0;
+// Question 3
+function question3() {
+  var car = prompt('I drive a Jeep. Yes or No?').toLowerCase();
+  var q3Right = 'You\'re right, I actually drive a Volkswagen.';
+  var q3Wrong = 'I don\'t, but Jeeps are still cool.';
+  checkAnswer(car, 'no', q3Right, q3Wrong);
+}
+question3();
+
+// Question 4
+function question4() {
+  var country = prompt('I have been to South America. Yes or No?').toLowerCase();
+  var q4Right = 'Correct! I\'ve been to Panama, Peru, and Mexico.';
+  var q4Wrong = 'Wrong! I\'ve been to Panama, Peru, and Mexico.';
+  checkAnswer(country, 'yes', q4Right, q4Wrong);
+}
+question4();
+
+// Question 5
+function question5() {
+  var celebrity = prompt('I once went on a date with Gal Gadot. Yes or No?').toLowerCase();
+  var q5Right = 'Ok ' + username + ', you got me. But a guy can dream, right??';
+  var q5Wrong = 'Nope, but thanks for thinking I have a chance, ' + username + '!';
+  checkAnswer(celebrity, "no", q5Right, q5Wrong);
+}
+question5();
+
+// Question 6
+function question6() {
+  var attempts = 4;
+  while (attempts > 0)
+    {
+      var number = parseInt(prompt('I\'m thinking of a number between 1 and 10, what is it?'));
+      if (number === 8) {
+        alert('That\'s right!');
+        questions.push(6);
+        console.log('The user guessed correctly');
         break;
-      } else if(justice != heroes[i]) {
-        guesses--;
+      } else if (number > 8) {
+        alert('Too high!');
+        attempts--;
+        console.log('The user guessed ' + number + ', which is too high');
+      } else if (number < 8) {
+        alert('Too low!');
+        attempts--;
+        console.log('The user guessed ' +number + ', which is too low');
+      } else {
+        alert('Please enter a number');
+        attempts--;
+        console.log('The user entered an invalid string');
+      }
+      if (attempts === 0) {
+        alert('Oh no, you ran out of guesses!');
+        console.log('The user used up all their attempts');
+      }
+    }
+  }
+  question6();
+
+// Question 7
+
+function question7() {
+  var heroes = ['batman', 'superman', 'wonder woman', 'the flash'];
+  var guesses = 6;
+
+  while (guesses > 0) {
+      var justice = prompt('Can you guess one of my favorite members of the Justice League?').toLowerCase();
+
+      for (var i = 0; i < heroes.length; i++) {
+        if (justice === heroes[i]) {
+          alert('Correct! My favorites are Batman, Superman, Wonder Woman, and The Flash.');
+          questions.push(7);
+          console.log('User guessed ' + justice);
+          guesses = 0;
+          break;
+        }
+      }
+      guesses--;
+      if (guesses > 0) {
         alert('Please try again. You have ' + guesses + ' tries remaining');
         console.log('User guessed incorrectly and has ' + guesses + ' remaining');
-        break;
+      }
+
+      if (guesses === 0) {
+        alert('Oh no, you ran out of guesses! My favorites are Batman, Superman, Wonder Woman, and The Flash.');
+        console.log('User is out of guesses');
       }
   }
 }
 
-//For when the user runs out of guesses
-if(guesses === 0 && justice != heroes[i] && justice != heroes[1] && justice != heroes[2] && justice != heroes[3]) {
-  alert('Oh no, you ran out of guesses! My favorites are Batman, Superman, Wonder Woman, and The Flash.')
-  console.log('User is out of guesses');
-}
+question7();
 
 // Total Score
-var total = questions.length;
-if(total === 7) {
-  alert('Thanks for playing, ' + username + '! You got ' + total +' out of 7 questions correct. Now please stop stalking me!');
-  console.log('User guessed ' + total + ' questions correctly.');
-} else {
-  alert('Thanks for playing, ' + username + '! You got ' + total +' out of 7 questions correct. Hopefully you learned more about me!');
-  console.log('User guessed ' + total + ' questions correctly');
+
+function totalScore() {
+  var total = questions.length;
+  if(total === 7) {
+    alert('Thanks for playing, ' + username + '! You got ' + total +' out of 7 questions correct. Now please stop stalking me!');
+    console.log('User guessed ' + total + ' questions correctly.');
+  } else {
+    alert('Thanks for playing, ' + username + '! You got ' + total +' out of 7 questions correct. Hopefully you learned more about me!');
+    console.log('User guessed ' + total + ' questions correctly');
+  }
 }
+
+totalScore();
